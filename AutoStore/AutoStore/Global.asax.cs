044d426a825cd141bce88686581bb9ed84a37c2a
Vasyl <vasyltanchinets98@gmail.com>
2019-05-07 21:43:51 +0300

    Creating and using a script package

--- AutoStore/AutoStore/Global.asax.cs
+++ AutoStore/AutoStore/Global.asax.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Optimization;
 using System.Web.Routing;
 using System.Web.Security;
 using System.Web.SessionState;
@@ -13,6 +14,7 @@ namespace AutoStore
         protected void Application_Start(object sender, EventArgs e)
         {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-        }        
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
     }
 }
