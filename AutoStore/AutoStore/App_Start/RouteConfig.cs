a4819736147a30386dc9366d757a5715930025e2
Vasyl <vasyltanchinets98@gmail.com>
2019-05-08 23:18:02 +0300

    Routes for administrative pages

--- AutoStore/AutoStore/App_Start/RouteConfig.cs
+++ AutoStore/AutoStore/App_Start/RouteConfig.cs
@@ -14,9 +14,12 @@ namespace AutoStore
             routes.MapPageRoute(null, "list/{page}", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "", "~/Pages/Listing.aspx");
             routes.MapPageRoute(null, "list", "~/Pages/Listing.aspx");
-            routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
 
+            routes.MapPageRoute("cart", "cart", "~/Pages/CartView.aspx");
             routes.MapPageRoute("checkout", "checkout", "~/Pages/Checkout.aspx");
+
+            routes.MapPageRoute("admin_orders", "admin/orders", "~/Pages/Admin/Orders.aspx");
+            routes.MapPageRoute("admin_autos", "admin/autos", "~/Pages/Admin/Autos.aspx");
         }
     }
 }
