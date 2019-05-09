2484e3db81a46368519f8cb3412e8044e4c3590e
Vasyl <vasyltanchinets98@gmail.com>
2019-05-09 21:24:11 +0300

    Adding a master page for administration

---
+++ AutoStore/AutoStore/Pages/Admin/Admin.Master.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AutoStore.Pages.Admin
+{
+    public partial class Admin : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public string OrdersUrl
+        {
+            get
+            {
+                return generateURL("admin_orders");
+            }
+        }
+        public string AutosUrl
+        {
+            get
+            {
+                return generateURL("admin_autos");
+            }
+        }
+
+        private string generateURL(string routeName)
+        {
+            return RouteTable.Routes.GetVirtualPath(null, routeName, null).VirtualPath;
+        }
+    }
+}
