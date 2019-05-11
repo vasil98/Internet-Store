1299db099ca56f5129ffff1ab094d46b824530d6
Vasyl <vasyltanchinets98@gmail.com>
2019-05-11 16:43:12 +0300

    Creating a WebForm for the administrator login

---
+++ AutoStore/AutoStore/Pages/Login.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Admin/Admin.Master" AutoEventWireup="true" 
+    CodeBehind="Login.aspx.cs" Inherits="AutoStore.Pages.Login" %>
+
+<asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:ValidationSummary ID="ValidationSummary1" runat="server" DisplayMode="SingleParagraph" CssClass="error" />
+
+    <div class="loginContainer">
+        <div>
+            <label for="name">Логін:</label>
+            <input name="name" />
+        </div>
+        <div>
+            <label for="password">Пароль:</label>
+            <input type="password" name="password" />
+        </div>
+        <button type="submit">Увійти</button>
+    </div>
+</asp:Content>
---
+++ AutoStore/AutoStore/Pages/Login.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Security;
+
+namespace AutoStore.Pages
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack)
+            {
+                string name = Request.Form["name"];
+                string password = Request.Form["password"];
+                if (name != null && password != null && FormsAuthentication.Authenticate(name, password))
+                {
+                    FormsAuthentication.SetAuthCookie(name, false);
+                    Response.Redirect(Request["ReturnUrl"] ?? "/");
+                }
+                else
+                {
+                    ModelState.AddModelError("fail", "Логін або пароль не вірні." +
+                        " Будь-ласка введіть дані заново");
+                }
+            }
+        }
+    }
+}
